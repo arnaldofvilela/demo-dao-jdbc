24cf1081271f0640b733e1fd135fd39ab7a3a076
Arnaldo Neto <vilela-neto@hotmail.com>
2020-12-26 19:56:17 -0300

    findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -31,11 +31,18 @@ public class Program {
 		}
 		
 		
+		System.out.println("\n=== TEST 3: seller findAll =====");
+		list = sellerDao.findAll();
+		for(Seller obj : list) {
+			System.out.println(obj);
+		}
+		
+		
 	}
 }
 
 
-
+ 
 
 
 
